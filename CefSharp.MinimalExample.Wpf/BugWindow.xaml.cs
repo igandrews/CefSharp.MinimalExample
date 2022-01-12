f2897cbd2e28b7a29fcedbe5c905f7b50762d855
igandrews <andrews@infragistics.com>
2022-01-12 15:06:52 -0500

    Added BugWindow for CefSharp bug #3961

---
+++ CefSharp.MinimalExample.Wpf/BugWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    /// <summary>
+    /// Interaction logic for BugWindow.xaml
+    /// </summary>
+    public partial class BugWindow : Window
+    {
+        private CefSharp.Wpf.ChromiumWebBrowser browser;
+
+        public BugWindow()
+        {
+            InitializeComponent();
+
+            this.StaticBrowser.Child = new CefSharp.Wpf.ChromiumWebBrowser("http://www.msn.net");
+        }
+
+        private void OnAddBrowser(object sender, RoutedEventArgs e)
+        {
+            if (browser == null)
+            {
+                browser = new CefSharp.Wpf.ChromiumWebBrowser("http://www.msn.net");
+            }
+
+            this.BrowserSite.Child = browser;
+        }
+
+        private void OnRemoveBrowser(object sender, RoutedEventArgs e)
+        {
+            if (browser != null)
+            {
+                this.BrowserSite.Child = null;
+            }
+        }
+    }
+}
